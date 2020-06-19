e53593a1f8e4b40c2faa63202707d3b153b3b1af
Erick Matsen <ematsen@gmail.com>
2020-06-19 04:26:12 -0700

    progress towards using TF-only likelihood implementation

--- experiments/2020-06-18-getting-started/run.py
+++ experiments/2020-06-18-getting-started/run.py
@@ -21,32 +21,10 @@ tfd = tfp.distributions
 DATA_ROOT = "../../data/"
 NEWICK_FILE = DATA_ROOT + "wnv/wnv_seed_6.nwk"
 FASTA_FILE = DATA_ROOT + "wnv/wnv.fasta"
-
-tree, taxon_names = treeflow.tree_processing.parse_newick(NEWICK_FILE)
-topology = treeflow.tree_processing.update_topology_dict(tree["topology"])
-taxon_count = len(taxon_names)
-sampling_times = tf.convert_to_tensor(tree["heights"][:taxon_count], dtype=tf.float32)
-
-prior = tfd.JointDistributionNamed(
-    dict(
-        clock_rate=tfd.LogNormal(loc=0.0, scale=3.0),
-        pop_size=tfd.LogNormal(0.0, 3.0),
-        tree=lambda pop_size: ConstantCoalescent(taxon_count, pop_size, sampling_times),
-    )
-)
-
-q_prior = treeflow.model.construct_prior_approximation(prior)
-q_tree = treeflow.model.construct_tree_approximation(NEWICK_FILE)
-q = tfd.JointDistributionNamed(dict(tree=q_tree, **q_prior))
-q.sample()
-
-subst_model = treeflow.substitution_model.HKY()
-likelihood, instance = treeflow.beagle.log_prob_conditioned_branch_only(
-    FASTA_FILE,
-    subst_model,
-    # @Christiaan I assume that your convention regarding NT order is the same as that
-    # of BEAST.
-    frequencies=np.array(
+# @Christiaan I assume that your convention regarding NT order is the same as
+# that of BEAST.
+FREQUENCIES = (
+    np.array(
         [
             0.26744051135162256,
             0.22286688874964067,
@@ -54,27 +32,81 @@ likelihood, instance = treeflow.beagle.log_prob_conditioned_branch_only(
             0.23099127912752943,
         ]
     ),
-    kappa=14.52346114599242,
-    rescaling=True,
-    newick_file=NEWICK_FILE,
 )
-
-wrapped_likelihood = lambda z: likelihood(
-    treeflow.sequences.get_branch_lengths(z["tree"])
-    * tf.expand_dims(z["clock_rate"], -1)
-)
-wrapped_likelihood(q.sample())
+KAPPA = 14.52346114599242
 
 
-log_posterior = lambda **z: (prior.log_prob(z) + wrapped_likelihood(z))
-log_posterior(**q.sample())
+def build_q_and_log_posterior(use_libsbn):
+    tree, taxon_names = treeflow.tree_processing.parse_newick(NEWICK_FILE)
+    topology = treeflow.tree_processing.update_topology_dict(tree["topology"])
+    taxon_count = len(taxon_names)
+    sampling_times = tf.convert_to_tensor(
+        tree["heights"][:taxon_count], dtype=tf.float32
+    )
+
+    prior = tfd.JointDistributionNamed(
+        dict(
+            clock_rate=tfd.LogNormal(loc=0.0, scale=3.0),
+            pop_size=tfd.LogNormal(0.0, 3.0),
+            tree=lambda pop_size: ConstantCoalescent(
+                taxon_count, pop_size, sampling_times
+            ),
+        )
+    )
+
+    q_prior = treeflow.model.construct_prior_approximation(prior)
+    q_tree = treeflow.model.construct_tree_approximation(NEWICK_FILE)
+    q = tfd.JointDistributionNamed(dict(tree=q_tree, **q_prior))
+    q.sample()
+
+    subst_model = treeflow.substitution_model.HKY()
+    if use_libsbn:
+        likelihood, instance = treeflow.beagle.log_prob_conditioned_branch_only(
+            FASTA_FILE,
+            subst_model,
+            frequencies=FREQUENCIES,
+            kappa=KAPPA,
+            rescaling=True,
+            newick_file=NEWICK_FILE,
+        )
+    else:
+        tree, taxon_names = treeflow.tree_processing.parse_newick(NEWICK_FILE)
+        sequences, pattern_counts = treeflow.sequences.get_encoded_sequences(
+            FASTA_FILE, taxon_names
+        )
+        value = {"sequences": sequences, "weights": pattern_counts}
+
+        likelihood, instance = treeflow.sequences.log_prob_conditioned_branch_only(
+            value,
+            topology,
+            category_count=4,
+            subst_model=subst_model,
+            category_weights=tf.convert_to_tensor([1.0], dtype=tf.float32),
+            category_rates=tf.convert_to_tensor([1.0], dtype=tf.float32),
+            frequencies=FREQUENCIES,
+            kappa=KAPPA,
+        )
+
+    wrapped_likelihood = lambda z: likelihood(
+        treeflow.sequences.get_branch_lengths(z["tree"])
+        * tf.expand_dims(z["clock_rate"], -1)
+    )
+    wrapped_likelihood(q.sample())
+
+    log_posterior = lambda **z: (prior.log_prob(z) + wrapped_likelihood(z))
+    log_posterior(**q.sample())
+
+    return q, log_posterior
 
 
-TRIAL_COUNT = 10
+TRIAL_COUNT = 1
 USE_TF_PROFILER = False
+USE_LIBSBN = False
+
+q, log_posterior = build_q_and_log_posterior(USE_LIBSBN)
 
 if USE_TF_PROFILER:
-    tf.profiler.experimental.start('logdir')
+    tf.profiler.experimental.start("logdir")
 
 with click.progressbar(range(TRIAL_COUNT), label="Trials") as bar:
     for _ in bar:
